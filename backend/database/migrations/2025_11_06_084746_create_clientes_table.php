1c0303ca6a7533f94b047b08e9c1d282b192442b
Antonio Joaquin <antoniojoaquincarmona@gmail.com>
2025-11-06 11:50:43 +0100

    Montar laravel en base de datos creada en xampp

--- backend/database/migrations/2025_11_06_084746_create_clientes_table.php
+++ backend/database/migrations/2025_11_06_084746_create_clientes_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
             $table->string('nombre');
             $table->string('email')->unique();
             $table->string('telefono')->nullable();
-            $table->string('password'); // 
+            $table->string('password');
+            $table->string('pedidos')->nullable();
             $table->timestamps();
         });
     }
